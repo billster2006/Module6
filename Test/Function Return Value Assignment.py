6731e6dc5c0a560230df538129cfcfba0799c83b
billster2006 <59900365+billster2006@users.noreply.github.com>
2020-02-23 11:35:41 -0600

    Second assignment

--- Test/Function Return Value Assignment.py
+++ Test/Function Return Value Assignment.py
@@ -3,12 +3,19 @@ def hourly_employee_input():  # keyword def with function name then ():
     name = input("Please enter your name: ")  # user input string
     hours_worked = int(input("Please enter your hours worked: "))  # user input int, possible ValueError
     hourly_rate_pay = float(input("Please enter your rate of pay: "))  # user input float, possible ValueError
-    print(name, hours_worked, hourly_rate_pay, ".")  # print statement, minial formatting
+    return hours_worked, hourly_rate_pay
+
+
+def weekly_pay():
+    hours, pay = hourly_employee_input()
+    weekly_pay_amount = hours * pay
+    print(weekly_pay_amount)
 
 
 if __name__ == '__main__':
     try:  # check for ValueError
-        hourly_employee_input()  # function call
+        display_string = hourly_employee_input()  # function call, store in a variable
     except ValueError as err:
         print("ValueError encountered! ")
-
+    else:
+        print(display_string)  # print the result of the function
