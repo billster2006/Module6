44df178804338e7332d8031791068b60e30ef373
billster2006 <59900365+billster2006@users.noreply.github.com>
2020-02-25 20:48:45 -0600

    Function Parameter and Return Value Assignment

---
+++ more_functions/string_functions.py
@@ -0,0 +1,8 @@
+def multiply_string():
+    fav_class = input('Enter your favorite class: ')
+    n = int(input('Enter a number between 1-9: '))
+    print(fav_class * n)
+
+if __name__ == '__main__':
+    multiply_string()
+
---
+++ test_functions/test_string_functions.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_multiple_string(self):
+        with self.assertEqual(ValueError):
+
+
+if __name__ == '__main__':
+    unittest.main()
