e8dfc1c43778277654f5c93880d6352b831246ca
billster2006 <59900365+billster2006@users.noreply.github.com>
2020-02-25 21:10:36 -0600

    Function Default Values Assignment

--- more_functions/validate_input_in_functions.py
+++ more_functions/validate_input_in_functions.py
@@ -1,5 +1,8 @@
-def score_input()
+def score_input():
     test_name = input('What is the test name? ')
     test_score = int(input('What is the test score? '))
-        if test_score in (0-100):
-            return 'Invalid test score, try again!'
+    if 0 < test_score > 100:
+        print('Invalid test score, try again!')
+
+if __name__ == '__main__':
+    score_input()
